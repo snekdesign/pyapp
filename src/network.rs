3e5cde835196a865a2af3119edaa1b58055a28f3
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-08-04 16:49:27 +0800

    add support for api.github.com sources

--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,44 @@
 use std::io::Write;
 
 use anyhow::{bail, Context, Result};
+use reqwest::header::{ACCEPT, HeaderMap, HeaderName, HeaderValue, USER_AGENT};
 
 use crate::terminal;
 
 pub fn download(url: &String, writer: impl Write, description: &str) -> Result<()> {
-    let mut response =
-        reqwest::blocking::get(url).with_context(|| format!("download failed: {}", url))?;
+    let normalized_url = url.to_lowercase();
+    let mut headers = HeaderMap::new();
+    if normalized_url.starts_with("https://api.github.com/repos/") {
+        headers.insert(
+            USER_AGENT,
+            HeaderValue::from_static("rust-reqwest/0.12.4"),
+        );
+        headers.insert(
+            HeaderName::from_static("x-github-api-version"),
+            HeaderValue::from_static("2022-11-28"),
+        );
+        if normalized_url.starts_with(
+            "https://api.github.com/repos/adang1345/pythonwindows/contents/3.",
+        ) {
+            headers.insert(
+                ACCEPT,
+                HeaderValue::from_static("application/vnd.github.raw+json"),
+            );
+        } else if normalized_url.starts_with(
+            "https://api.github.com/repos/indygreg/python-build-standalone/releases/assets/",
+        ) {
+            headers.insert(
+                ACCEPT,
+                HeaderValue::from_static("application/octet-stream"),
+            );
+        }
+    }
+
+    let mut response = reqwest::blocking::Client::new()
+        .get(url)
+        .headers(headers)
+        .send()
+        .with_context(|| format!("download failed: {}", url))?;
 
     let pb = terminal::io_progress_bar(
         format!("Downloading {}", description),
