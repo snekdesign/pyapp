8f2931867b2d136af7b31234ec473b5c24a64509
snekdesign <104812819+snekdesign@users.noreply.github.com>
2024-11-04 19:50:15 +0800

    update user agent

--- src/network.rs
+++ src/network.rs
@@ -10,7 +10,7 @@ pub fn download(url: &String, writer: impl Write, description: &str) -> Result<(
     let mut headers = HeaderMap::new();
     headers.insert(
         USER_AGENT,
-        HeaderValue::from_static("rust-reqwest/0.12.8"),
+        HeaderValue::from_static("rust-reqwest/0.12.9"),
     );
     if normalized_url.starts_with(
         "https://api.github.com/repos/adang1345/pythonwindows/contents/3.",
